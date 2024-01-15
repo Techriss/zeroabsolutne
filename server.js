ce5a7137f32913c530dc531fbe3f9dc81913039e
Krzysztof Kowalski <kkwlski@gmail.com>
2024-01-15 02:29:06 +0100

    app engine base

--- server.js
+++ server.js
@@ -4,25 +4,33 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
-const folderPath = path.join(__dirname, 'ex');
+const folderPath = path.join(__dirname, 'public', 'ex');
 
-app.use(express.static('public'));
-
-app.get('/files', (req, res) => {
-  fs.readdir(folderPath, (err, files) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Błąd odczytu folderu z plikami');
-    }
-
-    res.json({files});
-  });
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Serwer działa na http://localhost:${port}`);
+app.get('/files', (req, res) => {
+	fs.readdir(folderPath, (err, files) => {
+		if (err) {
+			console.error(err);
+			return res.status(500).send('Błąd odczytu folderu z plikami');
+		}
+
+		res.json({files});
+	});
+});
+
+app.use(function(req,res) {
+    res.status(404).render(path.join(__dirname, 'public', '404.html'));
+});
+
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
